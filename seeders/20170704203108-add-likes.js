42988a64ed1b043c4367ee25f4ec1d841dd82f03
Jason Tuttle <jason@hearspotbark.com>
2018-01-13 10:53:40 -0500

    updating seeders to actually work

--- seeders/20170704203108-add-likes.js
+++ seeders/20170704203108-add-likes.js
@@ -1,29 +1,28 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
+  up: function(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('likes', [
       {
         userId: 1,
-        messageId: 1
+        messageId: 1,
       },
       {
         userId: 2,
-        messageId: 1
+        messageId: 1,
       },
       {
         userId: 3,
-        messageId: 1
+        messageId: 1,
       },
       {
         userId: 4,
-        messageId: 1
-      }
-    ], {});
-
+        messageId: 1,
+      },
+    ]);
   },
 
-  down: function (queryInterface, Sequelize) {
+  down: function(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('likes', null, {});
-  }
+  },
 };
