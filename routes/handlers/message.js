81cb8e57c0905a224cc04c34c909034c125ad6e0
Jason Tuttle <jason@hearspotbark.com>
2018-01-14 10:14:56 -0500

    updating routes

--- routes/handlers/message.js
+++ routes/handlers/message.js
@@ -7,50 +7,67 @@ const models = require('../../models');
 
 router.get('/message/:id', function(req, res) {
   if (req.session.loggedIn) {
-    models.messages.find({
-      where: {id: req.params.id},
-      include: [
-        {
-          model: models.users,
-          as: 'user',
-          attributes: {exclude: ['password']}
-        },
-        {
-          model: models.likes,
-          as: 'likes',
-          include: [{
+    models.messages
+      .find({
+        where: { id: req.params.id },
+        include: [
+          {
             model: models.users,
             as: 'user',
-            attributes: {exclude: ['password']}
-          }]
-        }
-      ]
-    }).then(function(msg) {
-      const theMessage = {
-        message: msg.message,
-        user: msg.user.displayname,
-        createdAt: moment(msg.createdAt).fromNow(),
-      };
-      res.render('message', { message: theMessage, likes: msg.likes, loggedIn: req.session.loggedIn });
-    });
+            attributes: { exclude: ['password'] },
+          },
+          {
+            model: models.likes,
+            as: 'likes',
+            include: [
+              {
+                model: models.users,
+                as: 'user',
+                attributes: { exclude: ['password'] },
+              },
+            ],
+          },
+          {
+            model: models.comments,
+            as: 'comments',
+            include: [
+              {
+                model: models.users,
+                as: 'user',
+                attributes: { exclude: ['password'] },
+              },
+            ],
+          },
+        ],
+      })
+      .then(function(msg) {
+        const theMessage = {
+          message: msg.message,
+          user: msg.user.displayname,
+          createdAt: moment(msg.createdAt).fromNow(),
+        };
+        res.render('message', { message: theMessage, likes: msg.likes, loggedIn: req.session.loggedIn });
+      });
   } else {
     res.redirect('/login');
   }
 });
 
-router.post('/message/:id/delete', function(req, res) {
-  models.messages.find({
-    where: {id: req.params.id},
-    include: [
-      {
-        model: models.likes,
-        as: 'likes',
-      }
-    ]
-  }).then(function(message) {
-    message.destroy();
-    res.redirect('/');
-  });
+router.delete('/message/:id/delete', function(req, res) {
+  models.messages
+    .find({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: models.likes,
+          as: 'likes',
+        },
+      ],
+    })
+    .then(function(message) {
+      message.destroy();
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
--- routes/handlers/root.js
+++ routes/handlers/root.js
@@ -7,51 +7,64 @@ const models = require('../../models');
 
 router.get('/', function(req, res) {
   if (req.session.loggedIn) {
-    models.messages.findAll({
-      include: [
-        {
-          model: models.users,
-          as: 'user'
-        },
-        {
-          model: models.likes,
-          as: 'likes',
-          include: [{
+    models.messages
+      .findAll({
+        include: [
+          {
             model: models.users,
             as: 'user',
-            attributes: {exclude: ['password']}
-          }]
-        }
-      ],
-      order: Sequelize.col('createdAt')
-    }).then(function(msgs) {
-      let allMessages = msgs.map(function(obj, i) {
-        const time = moment(obj.createdAt).fromNow();
-        const thisMsg = {
-          id: obj.id,
-          message: obj.message,
-          createdAt:time,
-          user: obj.user,
-          likes: obj.likes,
-          isAuthor: (obj.user.id === req.session.user.id),
-          isLiked: (obj.likes.some(thing => thing.userId === req.session.user.id))
-        };
-        return thisMsg;
+          },
+          {
+            model: models.likes,
+            as: 'likes',
+            include: [
+              {
+                model: models.users,
+                as: 'user',
+                attributes: { exclude: ['password'] },
+              },
+            ],
+          },
+          {
+            model: models.comment,
+            as: 'comments',
+          },
+        ],
+        order: Sequelize.col('id'),
+      })
+      .then(function(msgs) {
+        let allMessages = msgs.map(function(obj, i) {
+          const time = moment(obj.createdAt).fromNow();
+          const thisMsg = {
+            id: obj.id,
+            message: obj.message,
+            createdAt: time,
+            user: obj.user,
+            likes: obj.likes,
+            comments: obj.comments,
+            isAuthor: obj.user.id === req.session.user.id,
+            isLiked: obj.likes.some(thing => thing.userId === req.session.user.id),
+          };
+          return thisMsg;
+        });
+        res.render('index', { messages: allMessages, user: req.session.user, loggedIn: req.session.loggedIn });
       });
-      res.render('index', { messages: allMessages, user: req.session.user, loggedIn: req.session.loggedIn });
-    });
   } else {
     res.redirect('/login');
   }
 });
 
 router.post('/like/:id', function(req, res) {
-  models.likes.findOrCreate({where: {
-    userId: req.session.user.id,
-    messageId: req.params.id
-  }}).then(function(record) {
-    res.redirect('/');
-  });
+  models.likes
+    .findOrCreate({
+      where: {
+        userId: req.session.user.id,
+        messageId: req.params.id,
+      },
+    })
+    .then(function(record) {
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
