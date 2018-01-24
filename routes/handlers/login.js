8343ab3c8b61a1df5b19956e5101df8849361e23
Jason Tuttle <jason@hearspotbark.com>
2018-01-24 09:07:18 -0500

    update login route for json response

--- routes/handlers/login.js
+++ routes/handlers/login.js
@@ -11,30 +11,36 @@ router.get('/login', function(req, res) {
 
 // handle form submission
 router.post('/login', function(req, res) {
-  if (req.body.button === "signup") { // if user hits the signup button
-    res.redirect('signup');           // send them to the signup page
-  } else {                            // otherwise...
+  if (req.body.button === 'signup') {
+    // if user hits the signup button
+    res.redirect('signup'); // send them to the signup page
+  } else {
+    // otherwise...
     const { username, password } = req.body; // store the values passed in
     req.checkBody('username', 'please enter a username').notEmpty();
     req.checkBody('password', 'please enter a password').notEmpty();
     req.getValidationResult().then(function(result) {
-      if (result.isEmpty()) { // .isEmpty:true means no validation errors...
-        models.users.findOne({ where: {username: username}}) // check users table
+      if (result.isEmpty()) {
+        // .isEmpty:true means no validation errors...
+        models.users
+          .findOne({ where: { username: username } }) // check users table
           .then(function(user) {
-            if (user) {  // if findOne() returned something not NULL
-              if (password === user.password) { // check if the passwords match
-                req.session.loggedIn = true;  // set session variable to true if so
-                req.session.user = {name: username, displayname: user.displayname, id: user.id };
-                res.redirect('/');  // and redirect to '/'
+            if (user) {
+              // if findOne() returned something not NULL
+              if (password === user.password) {
+                // check if the passwords match
+                req.session.loggedIn = true; // set session variable to true if so
+                req.session.user = { name: username, displayname: user.displayname, id: user.id };
+                res.status(200).json({ success: 'user logged in' });
               } else {
-                res.render('login', {error: [{msg: 'username and password don\'t match'}], loggedIn: req.session.loggedIn});
+                res.status(401).json({ error: "username and password don't match" });
               }
             } else {
-              res.render('login', {error: [{msg: 'username not found. click signup to create one!'}], loggedIn: req.session.loggedIn});
+              res.status(403).json({ error: 'username not found' });
             }
           });
       } else {
-        res.render('login', {error: result.array()[0], loggedIn: req.session.loggedIn});
+        res.status(400).json({ error: result.array() });
       }
     });
   }
