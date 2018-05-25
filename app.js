fe3e33c9819b9e133228ee86f02965aded4c1126
Jason Tuttle <jason@hearspotbark.com>
2018-05-25 16:36:56 -0400

    organization, add cors

--- app.js
+++ app.js
@@ -2,27 +2,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools');
+const typeDefs = require('./graphql/typedefs');
+const resolvers = require('./graphql/resolvers');
 
 const path = require('path');
 const ihdRouter = require('./routes/ihdRouter');
 const session = require('express-session');
 const expressValidator = require('express-validator');
+const cors = require('cors');
 
 const app = express();
 
-const typeDefs = require('./graphql/typedefs');
 
 // The root provides a resolver function for each API endpoint
-const resolvers = require('./graphql/resolvers');
 
 var schema = makeExecutableSchema({typeDefs, resolvers});
 // addMockFunctionsToSchema({ schema, mocks });
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+app.use('/graphql',
+  bodyParser.json(),
+  graphqlExpress({ schema })
+);
+
 app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(expressValidator());
+app.use('*', cors({ origin: 'http://localhost:3000'}))
 app.use('/resources', express.static(path.join(__dirname, 'public')));
 
 app.use(
